ac1d1c30d9eedef44a80541f4ece21073d35806c
necip homeoffice <necips@live.de>
2024-03-10 09:40:39 +0100

    update Chart3D

--- moduls/chart3D.js
+++ moduls/chart3D.js
@@ -1,37 +1,60 @@
 class Chart3D {
 
-    constructor(scene, data) {
+    constructor(scene, material, fontData, data) {
         this.scene = scene;
+        this.material = material;
+        this.fontData = fontData;
         this.update(data);
     }
 
     update(dataAll) {
         let data = dataAll.data;
         let scene = this.scene;
+        let fontData = this.fontData;
 
-        const material = new BABYLON.StandardMaterial("mat", scene);
-        material.emissiveColor = BABYLON.Color3.Gray(); // Green();
-        // material.wireframe = true;
+
+        let material = this.material;
+        if (material === null) {
+            material = new BABYLON.StandardMaterial("mat", scene);
+            material.emissiveColor = BABYLON.Color3.Black();
+        }
+
+        let materialLabel = new BABYLON.StandardMaterial("mat", scene);
+        materialLabel.emissiveColor = BABYLON.Color3.White();
+
 
         const size = 1;
         // const height = 0.1;
         let meshes = [];
 
 
-        let createLabel = (xOffset, yOffset, zOffset, fontSize, text) => {
+        let createLabel = (xOffset, yOffset, zOffset, fontSize, text, rotate) => {
             const align_h = "center";
             const align_v = "center";
             // let fontSize = 12;
 
-            const meshLabel = BABYLON.MeshBuilder.CreatePlane("", { height: 0.8 * size, width: 0.8 * size, sideOrientation: BABYLON.Mesh.DOUBLESIDE });
-            meshLabel.position.x = xOffset;
-            meshLabel.position.z = zOffset;
-            meshLabel.position.y = yOffset;
-            meshLabel.rotation.x = 0.5 * Math.PI;
-            meshLabel.material = create_MultiLineText_Material(scene, meshLabel, text, align_h, align_v, fontSize);
-            meshes.push(meshLabel);
+            // const meshLabel = BABYLON.MeshBuilder.CreatePlane("", { height: 0.6 * size, width: 0.6 * size, sideOrientation: BABYLON.Mesh.DOUBLESIDE });
+            // meshLabel.position.x = xOffset;
+            // meshLabel.position.z = zOffset;
+            // meshLabel.position.y = yOffset + 0.1;
+            // meshLabel.rotation.x = 0.5 * Math.PI;
+            // meshLabel.material = create_MultiLineText_Material(scene, meshLabel, text, align_h, align_v, fontSize);
+            // meshes.push(meshLabel);
+            // return meshLabel;
 
-            return meshLabel;
+            let myText = BABYLON.MeshBuilder.CreateText("myText", text, fontData, {
+                size: 6, // fontSize, // 16,
+                resolution: 64,
+                depth: 1
+            });
+            myText.scaling.scaleInPlace(0.05);
+            myText.position.x = xOffset;
+            myText.position.z = zOffset;
+            myText.position.y = yOffset + 0.1;
+            if (rotate)
+                myText.rotation.x = 0.5 * Math.PI;
+            myText.material = materialLabel;
+            return myText;
         }
 
         function mapRange(value, fromMin, fromMax, toMin, toMax) {
@@ -73,7 +96,7 @@ class Chart3D {
 
         let minMaxValues = findMinMax(data);
         let sum = sumValues(data);
-        createLabel(-1, 0.1, -1, 14, sum.toFixed(0));
+        createLabel(-1, 0.1, -1, 8, sum.toFixed(0), true);
 
         let colorEdge = new BABYLON.Color4(0, 0, 1, 1);
         let xValues = [];
@@ -109,19 +132,22 @@ class Chart3D {
             mesh.enableEdgesRendering();
             mesh.edgesWidth = 1.0;
             mesh.edgesColor = colorEdge;
+
+            mesh.receiveShadows = true;
+
             meshes.push(mesh);
 
-            createLabel(xOffset, boxHeight * heightFactor + 0.01, zOffset, 14, d.value.toFixed(0));
+            createLabel(xOffset, boxHeight * heightFactor + 0.01, zOffset, 8, d.value.toFixed(0), false);
         })
 
 
 
         xValues.forEach((value, index) => {
-            createLabel(index, 0.1, -1, 10, value);
+            createLabel(index, 0.1, -1, 8, value, true);
         });
 
         zValues.forEach((value, index) => {
-            createLabel(-1, 0.1, index, 10, value);
+            createLabel(-1, 0.1, index, 8, value, true);
         })
 
         this.meshes = meshes;   // public
